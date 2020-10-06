98a8e80a0f3c6ddf62eefcf064b630e14d48c40c
cmeyrueix4 <cmeyrueix44@gmail.com>
2020-10-06 15:16:00 -0700

    Integrating Mongoose

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const feedRoutes = require('./routes/feed');
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -15,4 +16,9 @@ app.use((req, res, next) => {
 
 app.use('/feed', feedRoutes);
 
-app.listen(8080);
+mongoose.connect('')
+.then(result => {
+    app.listen(8080);
+}).catch(err => {
+    console.log(err);
+})
