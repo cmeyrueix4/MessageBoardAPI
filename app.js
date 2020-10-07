ea9375aa8bc735aab1e9fbe4d7af779084056444
cmeyrueix4 <cmeyrueix44@gmail.com>
2020-10-07 16:54:47 -0700

    Added error handling and single post viewing

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const feedRoutes = require('./routes/feed');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -16,7 +18,14 @@ app.use((req, res, next) => {
 
 app.use('/feed', feedRoutes);
 
-mongoose.connect('')
+app.use((error, req, res, next) => {
+    console.log(error);
+    const status = error.statusCode || 500;
+    const message = error.message;
+    res.status(status).json({message: message});
+})
+
+mongoose.connect('mongodb://store-manager:manaGEME173@cluster0-shard-00-00.zurim.mongodb.net:27017,cluster0-shard-00-01.zurim.mongodb.net:27017,cluster0-shard-00-02.zurim.mongodb.net:27017/messageboard?ssl=true&replicaSet=atlas-96hs71-shard-0&authSource=admin&w=majority')
 .then(result => {
     app.listen(8080);
 }).catch(err => {
--- controllers/feed.js
+++ controllers/feed.js
@@ -3,27 +3,22 @@ const Post = require('../models/post');
 
 
 exports.getPosts = (req, res, next) => {
-    res.status(200).json({
-        posts: [{ 
-            _id: '1',
-            title: 'First Post', 
-            content: 'This is the first post!' , 
-            imageUrl: 'images/book.jpg', 
-            creator: {
-                name: 'Cyrielle'
-            },
-            createdAt: new Date(),
-        }]
+    Post.find().then(posts => {
+        res.status(200).json({message: 'Posts recieved', posts: posts});
+    }).catch(err => {
+        if(!err.statusCode){
+            err.statusCode = 500;
+        }
+        next(err);
     });
 };
 
 exports.createPost = (req, res, next) => {
     const errors  = validationResult(req);
     if(!errors.isEmpty()){
-        return res.status(422).json({
-            message: "Validation failed. Incorrect data.",
-            errors: errors.array()
-        });
+        const error = new Error('Validation failed. Incorrect data.');
+        error.statusCode = 422;
+        throw error;
     }
     const title = req.body.title;
     const content = req.body.content;
@@ -40,7 +35,29 @@ exports.createPost = (req, res, next) => {
             message: 'Post created successfully!',
             post: result
         });
-    }).catch(err => console.log(err));
-    // Create post in db
-    
+    }).catch(err => {
+        if(!err.statusCode){
+            err.statusCode = 500;
+        }
+        next(err);
+    });
 };
+
+exports.getPost = (req, res, next) => {
+    const postId = req.params.postId;
+    Post.findById(postId).then(post =>{
+        if(!post){
+            const error = new Error('Could not find post.');
+            error.statusCode = 404;
+            throw error;
+        }
+
+        res.status(200).json({message: 'Post fetched.', post: post});
+
+    }).catch(err => {
+        if(!err.statusCode){
+            err.statusCode = 500;
+        }
+        next(err);
+    });
+}
