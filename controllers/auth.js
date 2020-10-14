dc8af944652916b2252e305181f862af890718f8
cmeyrueix4 <cmeyrueix44@gmail.com>
2020-10-14 11:29:23 -0700

    Sign up and login working

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,7 @@
 const User = require('../models/user');
 const {validationResult} = require('express-validator/check');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 exports.signup = (req, res, next) => {
     const errors  = validationResult(req);
@@ -14,5 +16,54 @@ exports.signup = (req, res, next) => {
     const name = req.body.name;
     const password = req.body.password;
 
+    bcrypt.hash(password, 12).then(hashedPW => {
+        const user = new User({
+            email: email, 
+            name: name,
+            password: hashedPW 
+        });
+
+        return user.save();
+    }).then(result => {
+        res.status(201).json({message: 'User created', userId: result._id})
+    }).catch(err => {
+        if(!err.statusCode){
+            err.statusCode = 500;
+        }
+        next(err);
+    });
+}
+
+exports.login = (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    let loadUser;
+
+    User.findOne({email: email}).then(user =>{
+        if(!user){
+            const error = new Error('Email not found');
+            error.statusCode = 401;
+            throw error;
+        }
+
+        loadUser = user;
+
+        return bcrypt.compare(password, user.password);
+
+    }).then(isEqual => {
+        if(!isEqual){
+            const error = new Error('Invalid password');
+            error.statusCode = 401;
+            throw error;
+        }
+
+        const token = jwt.sign({email: loadUser.email, userId: loadUser._id.toString()}, 'secret', {expiresIn: '1h'});
+        res.status(200).json({token: token, userId: loadUser._id.toString()});
+    }).catch(err => {
+        if(!err.statusCode){
+            err.statusCode = 500;
+        }
+        next(err);
+    })
 
 }
